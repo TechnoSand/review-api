31d5b90ad635473c0e1af11dd6d09c6e217ae248
mateus <mateus.borja@ufpi.edu.br>
2025-04-30 19:13:54 -0300

    feat: adding mongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import routes from './routes.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 
 class App {
   constructor() {
     this.server = express();
-    
+
+    mongoose.connect(process.env.MONGO_URI);
+
     this.middlewares();
     this.routes();
   }
